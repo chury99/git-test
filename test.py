58f6a66735becd6d9f450789be328587a504b06e
chury99 <127005453+chury99@users.noreply.github.com>
2023-03-06 10:50:41 +0900

    json 사용법 추가

--- test.py
+++ test.py
@@ -1,2 +1,18 @@
 s_a = 'hello world'
 print(s_a)
+
+s = '{"a": "1", "b": "one"}'
+# json 파일 저장
+with open('test.json', 'wt') as file:
+    json.dump(s, file, indent=4, sort_key=True)
+
+# json to 메모리(dict)
+dic = json.dumps(s)
+
+# jeon 파일 읽기
+with open('test.json', 'rt') as file:
+    s = json.load(file)
+
+# json from 메모리(str)
+s = json.loads(dic)
+    
